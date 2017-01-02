96d762324d2e0229b4090f49b819f6a697c629a5
Duc Anh Babim <ducanh.babim@yahoo.com>
2017-01-03 02:09:17 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -12,10 +12,10 @@ fi
 
 # Prepare DNS
 if [ -f "/opt/kerio/mailserver/mailserver" ]; then
-HOSTNAME=$(hostname -s)
-DOMAIN=$(hostname -d)
-CONTAINERIP=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
-DNSSERVER=${DNSSERVER:-8.8.8.8}
+export HOSTNAME=$(hostname -s)
+export DOMAIN=$(hostname -d)
+export CONTAINERIP=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
+export DNSSERVER=${DNSSERVER:-8.8.8.8}
 # Set DNS Server to localhost
 echo "nameserver 127.0.0.1" > /etc/resolv.conf
 echo "nameserver $DNSSERVER" >> /etc/resolv.conf
